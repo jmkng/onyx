9baabc7ec2c9bb60d7bc6b97ebbf3158c9e31829
Jon Koenig <jkoenig@outlook.com>
2022-12-17 16:23:28 -0700

    Added Marshal function.

--- convert/yaml/yaml.go
+++ convert/yaml/yaml.go
@@ -4,7 +4,15 @@ import (
 	_yaml "gopkg.in/yaml.v3"
 )
 
-func Unmarshal(text string, out any) error {
-	err := _yaml.Unmarshal([]byte(text), out)
-	return err
+func Unmarshal(data []byte, out any) error {
+	return _yaml.Unmarshal(data, out)
+}
+
+func Marshal(data any) ([]byte, error) {
+	bytes, err := _yaml.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
